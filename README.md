f9ea7ed44ea5ab6f8ae0cbb81d469d39830fe576
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-08-03 23:46:43 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mini Cryptocurrency App
+
+This project uses [Coingecko API](https://www.coingecko.com/en/api) under the hood.
+
+Demo: [Click to see DEMO](https://cryptoduck-9c91d.web.app/)
+
+### Tools
+
+- React
+- Tailwind CSS
+- Firebase
+- Axios
+- React Icons
+- React Sparklines
+- DOMPurity
---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./context/ThemeContext";
+import Home from "./routes/Home";
+import SignIn from "./routes/Signin";
+import SignUp from "./routes/Signup";
+import Account from "./routes/Account";
+import CoinPage from "./routes/CoinPage";
+import axios from "axios";
+import Footer from "./components/Footer";
+import { AuthContextProvider } from "./context/AuthContext";
+
+function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+      // console.log(response.data);
+    });
+  }, [url]);
+
+  return (
+    <ThemeProvider>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home coins={coins} />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/Account" element={<Account />} />
+          <Route path="/coin/:coinId" element={<CoinPage />}>
+            <Route path=":coinId" />
+          </Route>
+        </Routes>
+        <Footer />
+      </AuthContextProvider>
+    </ThemeProvider>
+  );
+}
+export default App;
